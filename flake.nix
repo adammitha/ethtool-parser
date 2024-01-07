9d5139777ceefadab3fcff7f6cc501f0ea72c5da
Adam Mitha <adam.mitha@gmail.com>
2024-01-07 10:27:50 -0800

    Use rust-toolchain.toml instead of specifying rust version in flake.nix

--- flake.nix
+++ flake.nix
@@ -12,17 +12,7 @@
         rust-overlay.overlays.default
         (final: prev: {
           rustToolchain =
-            let
-              rust = prev.rust-bin;
-            in
-            if builtins.pathExists ./rust-toolchain.toml then
-              rust.fromRustupToolchainFile ./rust-toolchain.toml
-            else if builtins.pathExists ./rust-toolchain then
-              rust.fromRustupToolchainFile ./rust-toolchain
-            else
-              rust.stable.latest.default.override {
-                extensions = [ "rust-src" "rust-analyzer" ];
-              };
+              prev.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
         })
       ];
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
